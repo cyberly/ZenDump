367af31330556290c9d966734375ddfe0bff16e8
Chris Byerly <cbyerly@gmail.com>
2017-01-24 12:35:28 -0500

    added remaining necessary models (I think)

--- inc/models.inc.php
+++ inc/models.inc.php
@@ -10,4 +10,14 @@ class EndUser extends \Illuminate\Database\Eloquent\Model {
     public $timestamps = false;
     public $table = 'endusers';
 }
+
+class Meta extends \Illuminate\database\Eloquent\Model {
+    public $timestamps = false;
+    public $table = 'meta';
+}
+
+class Ticket extends \Illuminate\Database\Eloquent\Model {
+    public $timestamps = false;
+    public $table = 'tickets';
+}
  ?>
