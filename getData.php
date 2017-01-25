6135f378886c0f9d0f7dc31951e588e4cb551ce8
Chris Byerly <cbyerly@gmail.com>
2017-01-25 11:36:32 -0500

    shit is broken but I need to restart. add a switch in getdata nerd.

--- getData.php
+++ getData.php
@@ -19,3 +19,71 @@ include("inc/database.inc.php");
 include("inc/models.inc.php");
 
 set_time_limit(0);
+//Search params when I need it: type:ticket created>2016-01-01 fieldvalue:accnt*
+
+$prod = new zdCurl("production");
+//$search = "type:ticket created>2016-01-01 fieldvalue:accnt*?include=users";
+//$endpoint = "/search.json?query=" . urlencode($search);
+$searchId = "1277497"; //SFDC Demo #3
+//$searchId = "1277477"; //SFDC Demo #1
+$endpoint = "/tickets/$searchId/audits.json?include=users,groups,tickets";
+$data = $prod->get($endpoint)->response;
+
+//Let's build the ticket data.
+$ticketData = $data["tickets"][0];
+$ticketId = $ticketData["id"];
+$ticketChannel = $ticketData["via"]["channel"];
+if ($ticketChannel == "email"){
+    $ticketFrom = $ticketData["via"]["source"]["from"]["address"];
+}
+$ticketCreated = $ticketData["created_at"];
+$ticketUpdated = $ticketData["updated_at"];
+$ticketType = $ticketData["type"];
+$ticketSubject = $ticketData["subject"];
+
+//Let's build the end-user data.
+$users = $data["users"];
+foreach ($users as $user) {
+    if ($user["role"] == "end-user") {
+        //Instantiate DB lolz
+        //Put it in the DB if it doesn't exist.
+    }
+}
+//Here come das events. Run to the hills.
+
+$events = $data["audits"];
+foreach ($events as $event){
+    $eventId = $event["id"];
+    //$ticketId needs to go in the events table here as well.
+    $eventCreated = $event["created_at"];
+    echo "Event: $eventId, Created: $eventCreated", PHP_EOL;
+    foreach ($event["events"] as $action){
+        WRITE A SWITCH HERE YOU FUCKING IDIOT.
+        $actionId = $action["id"];
+        $actionType = $action["type"];
+        echo "\t$actionType", PHP_EOL;
+        if ($actionType == "Comment"){
+            $actionBody = $action["body"];
+            $actionPublic = $action["public"];
+            echo "\t\tPublic: $actionPublic", PHP_EOL;
+            echo "\t\tBody: " . strlen($actionBody), PHP_EOL;
+        } else {
+            $actionField = $action["field_name"];
+            $actionValue = $action["value"];
+            echo "\t\tField: $actionField, Value: $actionValue", PHP_EOL;
+            if ($actionType != "Create"){
+                $actionPrevious = $action["previous_value"];
+                echo "\t\tPrevious: $actionPrevious", PHP_EOL;
+            }
+        }
+
+
+    }
+}
+
+//var_dump($events);
+//$events = $eventsData["events"];
+//foreach ($events as $event){
+    //var_dump($event);
+//}
+//var_dump($userData);
--- migrations/20170120220355_zen_dump.php
+++ migrations/20170120220355_zen_dump.php
@@ -19,13 +19,6 @@ class ZenDump extends Migration
             //May need to add a column for groups, but will need a separate API call.
             //$table->timestamps();
         });
-        $this->schema->create('comments', function(Illuminate\Database\Schema\Blueprint $table){
-            $table->integer('id');
-            $table->string('name');
-            $table->string('email');
-            $table->primary('id');
-            //$table->timestamps();
-        });
 
         $this->schema->create('endusers', function(Illuminate\Database\Schema\Blueprint $table){
             $table->integer('id');
@@ -35,16 +28,47 @@ class ZenDump extends Migration
             //$table->timestamps();
         });
 
+        $this->schema->create('event_actions', function(Illuminate\Database\Schema\Blueprint $table){
+            $table->bigInteger('action_id');
+            $table->bigInteger('event_id');
+            $table->string('type');
+            $table->string('field_name');
+            $table->longText('value');
+            $table->longText('previous_value');
+            $table->longText('body');
+            $table->boolean('public');
+            $table->dateTime('created_at');
+            $table->bigInteger('author_id');
+            $table->primary('action_id');
+            //$table->timestamps();
+        });
+
         $this->schema->create('meta', function(Illuminate\Database\Schema\Blueprint $table){
-            $table->integer('id');
+            $table->integer('job_id');
+            $table->primary('job_id');
             //Not honestly sure what this is going ot be used for yet.
             //$table->timestamps();
         });
 
         $this->schema->create('tickets', function(Illuminate\Database\Schema\Blueprint $table){
-            $table->integer('id');
-            $table->string('name');
-            $table->primary('id');
+            $table->bigInteger('ticket_id');
+            $table->string('channel');
+            $table->string('recieved_from');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->string('type');
+            $table->longText('subject');
+            $table->primary('ticket_id');
+            //$table->timestamps();
+        });
+
+        $this->schema->create('ticket_events', function(Illuminate\Database\Schema\Blueprint $table){
+            $table->bigInteger('event_id');
+            $table->bigInteger('ticket_id');
+            $table->dateTime('created_at');
+            $table->string('channel');
+            $table->string('source_ip');
+            $table->primary('event_id');
             //$table->timestamps();
         });
 
