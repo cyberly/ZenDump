f305cc0c2b9bb1fc0fa406bff8e5d7dbb74492d6
cbyerly <cbyerly@liquidweb.com>
2017-05-31 15:49:00 -0400

    vars are hard

--- buildClosedList.php
+++ buildClosedList.php
@@ -54,7 +54,7 @@ $dateArray = array(
 foreach ($dateArray as $k => $v){
     $search = "type:ticket created>$k created<$v fieldvalue:accnt*" .
       " status:closed";
-    $endpoints[] = "/search.json?query=" . urlencode($search);
+    $endpoints = "/search.json?query=" . urlencode($search);
     $data = $prod->get($endpoint)->response;
     echo $data["count"];
 }
