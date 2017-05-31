93c7150c97b0137f60847229d1c146952fed8128
cbyerly <cbyerly@liquidweb.com>
2017-05-31 18:04:17 -0400

    lets try this

--- inc/threads.inc.php
+++ inc/threads.inc.php
@@ -97,12 +97,12 @@ class ListWork extends \Threaded{
             if ($prod->status != "200"){
                 if ($errorCount <= 4) {
                     Helper::saveError("soft", $this->endpoint, $prod->status);
-                    usleep(200000);
+                    usleep($defaultSleep);
                     $errorCount++;
                 } else {
                     Helper::saveError("hard", $this->endpoint, $prod->status);
                     $errorCount = 0;
-                    usleep(200000);
+                    usleep($defaultSleep);
                     continue;
                 }
             } else {
