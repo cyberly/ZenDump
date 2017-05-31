c0c0dda86f9db825e8edbf8f3241c9905515af9d
cbyerly <cbyerly@liquidweb.com>
2017-05-31 18:39:01 -0400

    ticket spacing

--- inc/threads.inc.php
+++ inc/threads.inc.php
@@ -89,7 +89,7 @@ class ListWork extends \Threaded{
         include("inc/models.inc.php");
         include("inc/helper.inc.php");
         $prod = new zdCurl("production");
-        $sleepDefault = 3000000;
+        $sleepDefault = 4000000;
         $errorCount = 0;
         while(!$this->lastPage){
             $reqStart = microtime(true);
