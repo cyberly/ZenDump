729d3f1b339410a0901ff216d1ba69d18fb3c057
cbyerly <cbyerly@liquidweb.com>
2017-06-07 16:12:42 -0400

    HURF FUCKING DURF

--- inc/threads.inc.php
+++ inc/threads.inc.php
@@ -92,7 +92,7 @@ class ListWork extends \Threaded{
         if ($this->listObj == "TicketsActive"){
             $sleepDefault = 0;
         } else {
-            $sleepDefault = 4000000;
+            $sleepDefault = 5000000;
             usleep(rand(0,15000000));
         }
         $errorCount = 0;
